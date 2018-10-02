97b750809b86e98e96f0355b86d6e0ab55ab6fd9
Siwalikm <siwalikm@gmail.com>
2018-10-02 17:35:21 +0530

    units

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
+const environment = process.env.EMBER_ENV;  
+const isTesting = environment === 'test';
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     sassOptions: {
       extension: 'scss'
-    }
+    },
+    hinting: !isTesting
   });
   return app.toTree();
 };
--- tests/unit/controllers/index-test.js
+++ tests/unit/controllers/index-test.js
@@ -1,12 +1,25 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
+
 module('Unit | Controller | index', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let controller = this.owner.lookup('controller:index');
     assert.ok(controller);
+  });
+
+  test('it should convert amount to currency  string', function(assert) {
+    const controller = this.owner.lookup('controller:index');
+    const numToCurrency = controller.get('numberToCurrency');
+    assert.equal(numToCurrency('2000', 'INR'), '₹ 2,000.00');
+  });
+
+
+  test('it`s return type should be array', function(assert) {
+    const controller = this.owner.lookup('controller:index');
+    // const selectedFlightsId = ;
+    assert.equal(controller.get('selectedFlightsId').__proto__.constructor.name, 'Array');
   });
 });
